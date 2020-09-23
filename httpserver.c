c5222e04b7517cbdbf00338438d5e8dc9cfafa92
Sinclair Liang <sinclairliang@gmail.com>
2020-09-23 15:10:05 -0500

    compiled

--- httpserver.c
+++ httpserver.c
@@ -3,3 +3,10 @@
 //
 
 #include "httpserver.h"
+#include <stdio.h>
+#define BUFFER_SIZE 32768 // size for 32Kib;
+#define HEADER_SIZE 4000  // size for 4Kib;
+
+int main(int argc, char *argv[]) {
+    fprintf(stdout, "%s", ":::: Waiting for new connection ::::\n");
+}
